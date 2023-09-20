f84a6360504f58ce8f560098c587b72836e9dfbd
JeffrinR <jeffrinromerom@gmail.com>
2023-09-19 19:13:15 -0600

    iconos camnbiados

--- src/components/juegos/Game.js
+++ src/components/juegos/Game.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Juego.css'
 
-const opciones = ['piedra', 'papel', 'tijera'];
+const opciones = ['piedra✊', 'papel✋', 'tijera✌'];
 
 function Game()  {
   const [userChoice, setUserChoice] = useState(null);
